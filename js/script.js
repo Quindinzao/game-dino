ba1a7c89e2c48f26bd73144fade5a73893bbf5d7
Joao Fernandes <joao.fernandes@impar.com.br>
2023-02-12 22:26:16 -0300

    end game

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
+const dinovana = document.querySelector(".love-of-my-life");
 const character = document.querySelector(".character");
 const cactus = document.querySelector(".cactus");
 const clouds = document.querySelector(".clouds");
+const score = document.querySelector(".score");
+const dialog = document.querySelector(".dialog");
+let scoreNumber = 0;
+let stop = false;
 
 const jump = () => {
   character.classList.add("jump");
   setTimeout(() => character.classList.remove("jump"), 600);
 };
 
+const scored = setInterval(() => {
+  if (!stop && scoreNumber < 21) {
+    scoreNumber = scoreNumber + 1;
+    score.innerHTML = "";
+    score.innerHTML = scoreNumber;
+  } else {
+    clearInterval(scored);
+  }
+}, 1000);
+
 const loop = setInterval(() => {
   const cactusPosition = cactus.offsetLeft;
   const cloudsPosition = clouds.offsetLeft;
   const characterPosition = +window.getComputedStyle(character).bottom.replace("px", "");
 
-  if (cactusPosition <= 116 && cactusPosition > 48 && characterPosition < 90) {
+  if (cactusPosition <= 124 && cactusPosition > 48 && characterPosition < 90) {
     cactus.style.left = `${cactusPosition}px`;
     cactus.style.animationName = "none";
 
@@ -26,7 +41,27 @@ const loop = setInterval(() => {
     character.src = "./images/game-over.png";
     character.style.marginRight = "12px";
 
+    stop = true;
     clearInterval(loop);
+  }
+
+  if (scoreNumber === 21) {
+    cactus.style.display = "none";
+    clouds.style.animationName = "none";
+
+    dinovana.style.display = "flex";
+    dinovana.style.animationName = "dinovana-animation";
+
+    character.style.width = "95px";
+    character.style.display = "flex";
+    character.style.left = "40%";
+    character.style.animationName = "character-animation";
+    character.src = "./images/game-over.png";
+
+    setInterval(() => {
+      dialog.style.display = "flex";
+      dialog.innerHTML = "Dinovana Limoni, meu amor por você é igual àquela estrela estranha que apareceu no céu: aumenta a cada dia que passa... Então, eu te pergunto: quer namorar comigo? :)";
+    }, 4000)
   }
 }, 10);
 
